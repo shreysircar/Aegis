7b3523c9f39e081edd96cda7965ac17047830bf4
shreysircar <sircarshrey@gmail.com>
2025-09-20 17:45:40 +0530

    changed colors

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF4A90E2); // cool blue
+  static const Color primary = Color(0x00ff5757); // coral red
   static const Color accent = Color(0xFF50E3C2);  // teal
   static const Color background = Color(0xFFF4F7FB);
   static const Color textPrimary = Colors.black87;
